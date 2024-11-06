677faa45ada2f51854b0d690fe857dcace70b8ec
cyy963 <cyrusw89@gmail.com>
2024-11-07 00:35:28 +0900

    update: Hero.jsx; removed border

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "../Components/Button";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w512 max-lg:max-w-388">
